e49525127eb3e32da96a47afac45197ae635a1f9
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2025-12-14 10:45:03 -0300

    teste 2

--- estruturaControle/doWhile.js
+++ estruturaControle/doWhile.js
@@ -1,13 +1,13 @@
 function getInteiroAleatorioEntre(min, max) {
-    const valor = Math.random() * (max, min) + min
-    return Math.floor(valor)
+  const valor = Math.random() * (max - min) + min;
+  return Math.floor(valor);
 }
 
-let opcao = -1
+let opcao //somnente inicializa a variável opcao sem atribuir valor
 
 do {
-    opcao = getInteiroAleatorioEntre(-1, 10)
-    console.log('Opção escolhida foi ' + opcao)
-} while (opcao != -1);
+  opcao = getInteiroAleatorioEntre(-1, 10); // gera um número aleatório entre -1 e 10
+  console.log("Opção escolhida foi " + opcao);
+} while (opcao != -1); // o bloco de código é executado pelo menos uma vez
 
-console.log('Até a Próxima!')
+console.log("Até a Próxima!");
