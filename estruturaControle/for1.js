a66d3a06a6d8e02980010bb865aa263dbf5ffd39
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2025-12-14 11:37:57 -0300

    finish estrutura de controle
    finalizando conteúdo de estrutura de controle

--- estruturaControle/for1.js
+++ estruturaControle/for1.js
@@ -1,15 +1,18 @@
+// exemplo com while
 let contador = 1
 while(contador <= 10) {
     console.log('contador = ' + contador)
     contador++
 }
 
+// exemplo com for
 for(let i = 1; i <= 10; i++) {
     console.log('i = ' + i) 
 }
 
-const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
-    for(let i = 0; - i < notas.length; i++) {
-        console.log('nota = ' + notas)
+// exemplo for com array 
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]  // array de notas
+for(let i = 0; i < notas.length; i++) {
+    console.log(`nota = ${notas[i]}`) // acessa cada elemento do array notas, usar crase para interpolar ``
+}
 
-    }
---
+++ estruturaControle/for2.js
@@ -0,0 +1,18 @@
+// for/in - estrutura de repetição para percorrer arrays ou objetos
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]  // array de notas
+for(let i in notas) {
+console.log(i, notas[i]) 
+}
+
+
+const pessoa = {
+    nome: 'Ana',
+    sobrenome: 'Silva',
+    idade: 29,
+    peso: 64
+}
+
+// for/in para objetos
+for(let atributo in pessoa) {
+    console.log(`${atributo} = ${pessoa[atributo]}`)
+}
