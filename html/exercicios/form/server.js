fd03dd44c5e049dc3e4cfe272a0bb00d92548f48
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2025-12-26 21:38:58 -0300

    form

--- html/exercicios/form/server.js
+++ html/exercicios/form/server.js
@@ -6,7 +6,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/usuarios", (req, resp) => {
   console.log(req.body);
-  resp.send("<h1>Parabéns</h1>");
+  resp.send("Parabéns. Usuario incluido");
 });
 
-app.listen(3003);
+app.post("/usuarios/:id", (req, resp) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  resp.send("Parabéns. Usuário alterado");
+});
+
+app.listen(5500);
